c63bfb19139926c2c407f4a4eb89b064bc7ef10c
vishalkumar74920 <115637835+vishalkumar74920@users.noreply.github.com>
2023-06-23 15:21:58 +0530

    Add files via upload

---
+++ display the sum of n natural number/display the sum of n natural number/Program.cs
@@ -0,0 +1,27 @@
+﻿// Display the sum of n natural number
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a positive integer:");
+        int n = int.Parse(Console.ReadLine());
+
+        int sum = CalculateSum(n);
+
+        Console.WriteLine("The sum of the first {0} natural numbers is: {1}", n, sum);
+    }
+
+    static int CalculateSum(int n)
+    {
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            sum += i;
+        }
+
+        return sum;
+    }
+}
