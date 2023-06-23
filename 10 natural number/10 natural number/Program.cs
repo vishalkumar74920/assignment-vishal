990eed106e15b4cbc7b29ac6f7bb403e099bf976
vishalkumar74920 <115637835+vishalkumar74920@users.noreply.github.com>
2023-06-23 14:30:32 +0530

    Add files via upload
    assignment completed

---
+++ 10 natural number/10 natural number/Program.cs
@@ -0,0 +1,16 @@
+﻿// display the first 10 natural number
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("The first 10 natural numbers are:");
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
+
